289d5484dd2332276dedfa39a0e46a3c109fa018
apalebluedot9283 <apalebluedot9283@gmail.com>
2024-05-20 18:20:13 +0900

    3-1-08.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ fifa2019=pd.read_csv('fifa2019.csv')
 print(fifa2019.shape)
 import matplotlib.pyplot as plt
 
-fifa2019['Preferred Foot'].value_counts().plot(kind='bar')
+fifa2019['Preferred Foot'].value_counts().plot(kind='pie',autopct='%1.f%%')
 plt.legend()
 plt.show()
