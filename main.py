4b3f0091bc5cbdedb62f83aaf84ee87c7427ff66
apalebluedot9283 <apalebluedot9283@gmail.com>
2024-05-20 18:35:23 +0900

    3-1-13.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pandas as pd
 fifa2019=pd.read_csv('fifa2019.csv')
-print(fifa2019.shape)
-import matplotlib.pyplot as plt
+df=pd.DataFrame.copy(fifa2019.sort_values(by='Overall',ascending=False).head(200))
+test_features=['Name','Stamina','Dribbling','ShortPassing','Penalties']
+test_df=pd.DataFrame(df,columns=test_features)
 
-fifa2019['Preferred Foot'].value_counts().plot(kind='pie',autopct='%1.f%%')
-plt.legend()
+import numpy as np
+XY=np.array(test_df)
+X=XY[:,1:3]
+K=3
+C_x=np.random.choice(X[:,0],K)
+C_y=np.random.choice(X[:,1],K)
+C=np.array(list(zip(C_x,C_y)))
+
+import matplotlib.pyplot as plt
+Stamina=test_df['Stamina']
+Dribbling=test_df['Dribbling']
+plt.title('Stamina&Dribbling')
+plt.xlabel('Stamina')
+plt.ylabel('Dribbling')
+plt.scatter(Stamina, Dribbling, marker='^',c='blue',s=10,label='player')
+plt.scatter(C_x,C_y,marker='*',s=200,c='black',label='centroids')
+plt.legend(loc='best')
+plt.grid()
 plt.show()
