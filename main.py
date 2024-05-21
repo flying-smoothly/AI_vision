9d7956c73cde23188f9ce41a9c36ae81df338d07
apalebluedot9283 <apalebluedot9283@gmail.com>
2024-05-22 00:30:41 +0900

    지능형전자시스템전공 졸업자분들 출력하기

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pandas as pd
+
 job=pd.read_csv('취업 데이터(2016-2021).csv')
 
 electronic_engineering=job['학과']=='지능형전자시스템전공'
 sub1=job.loc[electronic_engineering]
-print(electronic_engineering)
-print(sub1)
+
+import matplotlib.pyplot as plt
+
+sub1['company distinguish'].value_counts().plot(kind='pie',autopct='%1.f%%')
+plt.legend()
+plt.show()
